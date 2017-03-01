9ee9eab6eeb8278f3812f9c8b4a701a59837b987
Serge Matveenko <s@matveenko.ru>
2017-03-01 04:00:59 +0300

    Show off the home page.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,39 @@ import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux'
 import { Grid, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import logo from 'images/piterpy-logo.png';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootswatch/paper/bootstrap.css'
+import 'bootswatch/paper/bootstrap.css';
+import 'main.css'
 
 import store from 'store'
 import App from 'modules/App/components/App'
 
 const history = syncHistoryWithStore(browserHistory, store)
 
-ReactDOM.render((<Provider store={store}>
+ReactDOM.render((<Provider store={store}><div>
+	<Navbar collapseOnSelect>
+		<Navbar.Header>
+			<Navbar.Brand>
+				<a href="/"><img src={logo} alt="Piter Py" /></a>
+			</Navbar.Brand>
+			<Navbar.Toggle />
+		</Navbar.Header>
+		<Navbar.Collapse>
+			<Nav>
+				<NavItem href="#">About</NavItem>
+				<NavItem href="#">Code of Conduct</NavItem>
+				<NavItem href="#">Call for Papers</NavItem>
+				<NavItem href="#">Speakers</NavItem>
+				<NavItem href="#">Schedule</NavItem>
+				<NavItem href="#">Organizers & sponsors</NavItem>
+			</Nav>
+			<Nav pullRight>
+				<NavItem href="#">Org Zone</NavItem>
+			</Nav>
+		</Navbar.Collapse>
+	</Navbar>
 	<Grid>
 		<Row>
 			<Col md={12}>
@@ -22,4 +46,8 @@ ReactDOM.render((<Provider store={store}>
 			</Col>
 		</Row>
 	</Grid>
-</Provider>), document.getElementById('root'));
+	<footer className="ppy-footer">
+		<p>&copy; 2017 Piter Py</p>
+		<address>MIT Licensed. <a href="https://github.com/piterpy">Fork it!</a></address>
+	</footer>
+</div></Provider>), document.getElementById('root'));
--- src/modules/App/components/App.js
+++ src/modules/App/components/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
-import { } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+import { Jumbotron, Button, ResponsiveEmbed } from 'react-bootstrap';
+import banner from 'images/piterpy4.jpg';
 
 class App extends Component {
 	render() {
-		return (<div>Piter Py</div>);
+		return (<div>
+			<Jumbotron>
+				<h1>Piter Py #4 <small>June 9-10, Saint-Petersburg, Russia</small></h1>
+				<p>For the third year in a row IT-Sobytie holds Python-conference - «Piter Py», which will take place on 22 and 23 April in St. Petersburg.</p>
+				<p>The aim of the conference to bring together people who care about Python to communicate and share experiences.</p>
+				<p><Button bsStyle="primary">Register now</Button></p>
+			</Jumbotron>
+			<h2>We are happy to welcome everyone!</h2>
+			<Row>
+				<Col md={8} mdPush={4}>
+					<ResponsiveEmbed a16by9>
+						<iframe src="https://www.youtube.com/embed/videoseries?list=PL2lK53ydln6740IbnfOe51BZ5hFk4S9oo" allowFullScreen></iframe>
+					</ResponsiveEmbed>
+				</Col>
+				<Col md={4} mdPull={8}>
+					<big>These invitations are brought to you from organizers of Piter&nbsp;Py and our friends.</big>
+				</Col>
+			</Row>
+			<h2>Stay in touch</h2>
+			<Row>
+				<Col md={8}>
+					<a className="twitter-timeline" data-height="700" data-dnt="true" data-theme="light" href="https://twitter.com/PiterPy">Tweets by PiterPy</a>
+				</Col>
+				<Col md={4}>
+					<big>We are excited to here from you.</big>
+					<a href="https://twitter.com/intent/tweet?screen_name=piterpy" className="twitter-mention-button" data-size="large" data-related="" data-dnt="true" data-show-count="false">Tweet to @piterpy</a>
+				</Col>
+			</Row>
+		</div>);
     }
 }
 
